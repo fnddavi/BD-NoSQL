87e0c2bb1ba346104a18e976490957179a444a95
Fernando Davi <92491040+fnddavi@users.noreply.github.com>
2024-05-03 19:53:28 -0300

    update

--- Ativ4-mongodb/servidor/src/index.ts
+++ Ativ4-mongodb/servidor/src/index.ts
@@ -3,6 +3,8 @@ import cors from "cors";
 import dotenv from "dotenv";
 import routes from "./routes";
 import { connect } from "./models/connection";
+
+
 dotenv.config();
 // será usado 3000 se a variável de ambiente não tiver sido definida
 const PORT = process.env.PORT || 3000;
